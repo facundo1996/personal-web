da06c05b3a1fd06b625efe7debb7b23adbe24e20
facundo1996 <ffaacu1@gmail.com>
2024-01-03 12:16:01 -0300

    Fix project card

--- src/components/ProjectCard.vue
+++ src/components/ProjectCard.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="project-card border border-3 border-dark" :style="`background: url(${project.pictures[0]}); background-size: cover;`">
+  <div class="project-card border border-3 border-dark" :style="`background: url('${project.pictures[0]}'); background-size: cover;`">
     <div class="header-card" :class="project.primary === 'Vue JS' ? 'vue' : project.primary === 'React JS' ? 'react' : 'quasar'">
       {{ project.primary }}
     </div>
