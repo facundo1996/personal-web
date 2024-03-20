d0f63220a2a796546836476a196ec06ec3dc04d3
facundo1996 <ffaacu1@gmail.com>
2024-03-19 22:58:21 -0300

    WIP experience

---
+++ src/components/Experience.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="">
+
+    <div data-aos="fade-up" data-aos-duration="600" class="background-box-about-me mb-4">
+      <h5 class="subtitle">Experiencia Profesional</h5>
+
+      <a class="title-job" target="_blanck" href="https://www.softwareontheroad.com/">Software on the Road</a>
+
+      <p class="text">
+        Partner de desarrollo donde trabaje por mas de 3 años (2020-2023), en la cual desarolle tareas como <b>Front End</b> y partecipe en diversos proyectos como aplicaciones mobiles, proyectos crypto y paginas web.
+        <!-- Mi enfoque se centra en el desarrollo ágil, la maquetación de diseños y la adaptación de interfaces para
+        distintos dispositivos y tamaños de pantalla.
+        Poseo experiencia en la creación y optimización de componentes, así como en la conexión y uso de APIs para
+        lograr un rendimiento óptimo. -->
+      </p>
+    </div>
+
+  </div>
+</template>
--- src/pages/AboutMe.vue
+++ src/pages/AboutMe.vue
@@ -3,6 +3,7 @@
   <div class="d-flex flex-column justify-content-start align-items-center py-4 min-height-page">
     <div class="container-page">
       <AboutMeText />
+      <Experience />
       <Skills />
     </div>
   </div>
@@ -10,11 +11,13 @@
 
 <script lang="ts">
 import AboutMeText from '../components/AboutMeText.vue';
+import Experience from '../components/Experience.vue';
 import Skills from '../components/Skills.vue';
 export default {
   name: 'AboutMe',
   components: {
     AboutMeText,
+    Experience,
     Skills
   }
 }
