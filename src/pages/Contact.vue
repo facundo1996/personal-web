4e9d1fd7df6dfad927bc32f8b6c34181c3cf6a96
facundo1996 <ffaacu1@gmail.com>
2024-10-23 00:53:04 -0300

    Update contact form

--- src/pages/Contact.vue
+++ src/pages/Contact.vue
@@ -1,6 +1,6 @@
 <template>
-  <div data-aos="fade-zoom" :data-aos-duration="1000" class="d-flex flex-column justify-content-start align-items-center min-height-page bg-tiquewhite">
-
+  <div data-aos="fade-zoom" :data-aos-duration="1000"
+    class="d-flex flex-column justify-content-start align-items-center min-height-page bg-tiquewhite">
 
     <div class="container-page d-flex flex-column align-items-center justify-content-start">
 
@@ -11,23 +11,31 @@
         <form id="form">
           <div class="form-group">
             <label class="label-form" for="name">Nombre</label>
-            <input type="email" class="form-control font-text" id="name" v-model="name" name="name" placeholder="Facundo Benedetto">
+            <input required type="text" class="form-control font-text" id="name" v-model="name" name="name"
+              placeholder="Facundo Benedetto">
           </div>
 
           <div class="form-group mt-4">
             <label class="label-form" for="email">Email</label>
-            <input type="email" class="form-control font-text" id="email" v-model="email" name="email" placeholder="email@example.com">
+            <input required type="email" class="form-control font-text" id="email" v-model="email" name="email"
+              placeholder="email@example.com">
           </div>
 
           <div class="form-group mt-4">
             <label class="label-form" for="message">Mensaje</label>
-            <textarea class="form-control font-text" id="message" v-model="message" name="message" rows="3" style="min-height: 150px;"
-              placeholder="Mensaje ..."></textarea>
+            <textarea class="form-control font-text" id="message" v-model="message" name="message" rows="3"
+              style="min-height: 150px;" placeholder="Mensaje ..."></textarea>
+          </div>
+          
+          <div v-if="error" class="alert alert-danger text-center mt-3" role="alert">
+            {{ error }}
+          </div>
+          <div class="btn-form-container">
+            <!-- <div class="g-recaptcha" :data-sitekey="PUBLIC_RECAPTCHA"></div> -->
+            <button :disabled="buttonDisabled" type="submit" class="btn custom-button bg-white mt-4 px-5"
+              v-on:click="sendEmail">Enviar</button>
           </div>
         </form>
-        <div class="btn-form-container">
-          <button class="btn custom-button bg-white mt-4 px-5" v-on:click="sendEmail">Enviar</button>
-        </div>
       </div>
     </div>
 
@@ -38,22 +46,47 @@
 
 <script lang="ts">
 import emailjs from '@emailjs/browser';
+
 export default {
-  data(){
+  data() {
     return {
       name: "",
       message: "",
       email: "",
+      error: '',
+      SERVICE_ID: import.meta.env.VITE_CO_SERVICE_ID,
+      TEMPLATE_ID: import.meta.env.VITE_CO_TEMPLATE_ID,
+      KEY_PUBLIC: import.meta.env.VITE_CO_KEY_PUBLIC,
+      buttonDisabled: false,
+      PUBLIC_RECAPTCHA: import.meta.env.VITE_KEY_PUBLIC_RECAPTCHA,
+      RECAPTCHA: import.meta.env.VITE_KEY_RECAPTCHA,
+
     }
   },
   name: 'Contact',
+  mounted() {
+
+  },
   methods: {
-    sendEmail() {
-      emailjs.send("service_phjc9cs","template_c7p4cy8",{
+    sendEmail(e: any) {
+      
+      e.preventDefault()
+      const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+      if (!this.name) {
+        this.error = "El nombre no es válido."
+        return
+      }
+      if (!this.email || !emailPattern.test(this.email)) {
+        this.error = 'El email no es válido.'
+        return
+      }
+      this.error = ""
+      this.buttonDisabled = true
+      emailjs.send(this.SERVICE_ID,this.TEMPLATE_ID,{
         name: this.name,
         message: this.message,
         email: this.email
-      }, 'j-Uuf5D2B0nwery5p');
+      }, this.KEY_PUBLIC);
     }
   }
 
